4d9dced3eab5f137623eff8b74767b47744cee91
Paul Baumstark <41694868+pbmstrk@users.noreply.github.com>
2023-10-22 20:32:08 +0100

    expand docs in code

--- hndata-clj/src/hndata/http.clj
+++ hndata-clj/src/hndata/http.clj
@@ -8,20 +8,27 @@
   retryable-http-status-codes
   #{408 409 410 500 502 503 504 509})
 
-(defn should-retry? [exception]
+(defn should-retry?
+  "Determines if a request should be retried based on the exception received."
+  [exception]
   (cond
     (instance? SocketException exception) true
     (and (instance? ExceptionInfo exception)
          (retryable-http-status-codes (-> exception ex-data :status))) true
     :else false))
 
-(defn fetch-data [url params]
+(defn fetch-data
+  "Performs a GET request to the specified URL and returns the response body if the request is successful."
+  [url params]
   (let [response (client/get url {:as :json :query-params params})]
     (if (= 200 (:status response))
       (:body response)
       (throw (ex-info "Failed to fetch data" {:status (:status response)})))))
 
-(defn fetch-with-retries [url params retries]
+(defn fetch-with-retries
+  "Attempts to fetch data from a URL with specified parameters, retrying up to a specified number of
+  times if certain exceptions occur."
+  [url params retries]
   (try
     (fetch-data url params)
     (catch Exception e
@@ -32,5 +39,8 @@
           (fetch-with-retries url params (dec retries)))
         (throw e)))))
 
-(defn fetch-data-from-algolia [url params]
+(defn fetch-data-from-algolia
+  "Fetches data from Algolia with retry logic, given a URL and query parameters.
+  Retries up to 3 times upon failures that are deemed retry-able."
+  [url params]
   (fetch-with-retries url params 3))
--- hndata-clj/src/hndata/main.clj
+++ hndata-clj/src/hndata/main.clj
@@ -26,7 +26,9 @@
   (when (some? val)
     (str field (operator-mappings op) val)))
 
-(defn build-query-params [tags creation-lb creation-ub]
+(defn build-query-params
+  "Build query parameters for the Algolia API based on tags and creation date bounds."
+  [tags creation-lb creation-ub]
   (let [params {:tags tags :hitsPerPage hits-per-page}
         lower-bound (build-numeric-filter-string "created_at_i" "gt" creation-lb)
         upper-bound (build-numeric-filter-string "created_at_i" "lt" creation-ub)
@@ -49,25 +51,35 @@
          :created_at)
    record))
 
-(defn insert-with-preprocessing! [ds records query pre-processor]
-  (jdbc/execute-batch! ds query (map pre-processor records) {}))
+(defn insert-with-preprocessing!
+  "Execute a batch insert into the database after processing records."
+  [ds records query record-processor]
+  (jdbc/execute-batch! ds query (map record-processor records) {}))
 
-(defn insert-stories! [ds stories]
+(defn insert-stories!
+  "Batch insert stories into the database."
+  [ds stories]
   (insert-with-preprocessing! ds stories insert-stories-statement extract-story-metadata))
 
-(defn insert-comments! [ds comments]
+(defn insert-comments!
+  "Batch insert comments into the database."
+  [ds comments]
   (insert-with-preprocessing! ds comments insert-comments-statement extract-comment-metadata))
 
-(defn fetch-batch [search-map creation-lb creation-ub]
-  (let [query-params (build-query-params search-map creation-lb creation-ub)
+(defn fetch-batch
+  "Fetch a batch of results from API based on search parameters."
+  [tags creation-lb creation-ub]
+  (let [query-params (build-query-params tags creation-lb creation-ub)
         response (http/fetch-data-from-algolia algolia-hn-search-url query-params)]
     (log/info "Fetching batch with params.. " query-params)
     (:hits response)))
 
-(defn process-batch [process-function search-map creation-lb creation-ub]
-  (let [records (fetch-batch search-map creation-lb creation-ub)]
+(defn process-batch
+  "Process a batch of records and determine if further processing is needed."
+  [insert-function tags creation-lb creation-ub]
+  (let [records (fetch-batch tags creation-lb creation-ub)]
     (log/info "Processing batch of size: " (count records))
-    (process-function records)
+    (insert-function records)
     ; The created_at_i field is used to paginate through the results. If
     ; the number of results is equal to the page size (i.e. hits per page),
     ; this indicates we may need further requests to retrieve all the data
@@ -75,34 +87,33 @@
       {:should-continue true :ub (apply min (map :created_at_i records))}
       {:should-continue false})))
 
-(defn process-search-results [{:keys [tags creation-lb creation-ub process-function]}]
+(defn process-search-results [{:keys [tags creation-lb creation-ub insert-function]}]
   (loop [next-ub creation-ub iteration 1]
     (log/info "Batch" iteration)
-    (let [{:keys [should-continue ub]} (process-batch process-function tags creation-lb next-ub)]
+    (let [{:keys [should-continue ub]} (process-batch insert-function tags creation-lb next-ub)]
       (if should-continue
         (recur ub (inc iteration))
         (log/info "Fetching complete...")))))
 
-(defn get-unprocessed-ids [ds]
+(defn get-unprocessed-hiring-story-ids [ds]
   (let [query "select get_unprocessed_hiring_stories() as id;"
         object-ids (->> (sql/query ds [query])
                         (map :id))]
     object-ids))
 
 (defn process-all-comments [ds]
-  (let [ids (get-unprocessed-ids ds)]
+  (let [ids (get-unprocessed-hiring-story-ids ds)]
     (doseq [id ids]
-      (process-search-results {:tags             (format "comment,story_%s" id)
-                               :creation-lb      nil
-                               :creation-ub      nil
-                               :process-function (partial insert-comments! ds)})
+      (process-search-results {:tags            (format "comment,story_%s" id)
+                               :creation-lb     nil
+                               :creation-ub     nil
+                               :insert-function (partial insert-comments! ds)})
       (sql/insert! ds :hiring_posts_log {:objectID id}))))
 
 (defn fetch-latest-story-timestamp [ds]
   (let [record (sql/query ds ["select max(created_at_i) from stories"])]
     (when-let [timestamp (-> record first :max)]
       timestamp)))
-
 
 (defn refresh-views [ds views]
   (doseq [view views]
@@ -112,9 +123,11 @@
 (defn -main []
   (let [ds (get-datasource-from-env)
         latest-timestamp (fetch-latest-story-timestamp ds)]
-    (process-search-results {:tags             "story"
-                             :creation-lb      latest-timestamp
-                             :creation-ub      nil
-                             :process-function (partial insert-stories! ds)})
+    ; process all new stories from the latest timestamp record in db
+    (process-search-results {:tags            "story"
+                             :creation-lb     latest-timestamp
+                             :creation-ub     nil
+                             :insert-function (partial insert-stories! ds)})
+    ; check if there are any unprocessed "who is hiring" stories and fetch comments
     (process-all-comments ds)
     (refresh-views ds ["keywords" "hiring_keywords" "submissions_per_day"])))
