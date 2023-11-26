f44b931c61f8f980292f821eb0a2f1d88d3b086a
Paul Baumstark <41694868+pbmstrk@users.noreply.github.com>
2023-11-26 19:03:38 +0000

    fix script

--- hndata-clj/load-data.sh
+++ hndata-clj/load-data.sh
@@ -19,4 +19,4 @@ echo "Docker image built successfully."
 # Construct the JDBC URL
 DATABASE_URL="jdbc:postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@db:5432/hndata"
 
-docker run --network=hn-trends_default --rm -e database_url="$JDBC_DATABASE_URL" -e nyt_api_key="$nyt_api_key" hn-trends-load
+docker run --network=hn-trends_default --rm -e database_url="$DATABASE_URL" -e nyt_api_key="$nyt_api_key" hn-trends-load
