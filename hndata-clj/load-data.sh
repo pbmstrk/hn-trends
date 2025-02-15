3387539a6293296fdd3b4db88374b977512268af
Paul Baumstark <41694868+pbmstrk@users.noreply.github.com>
2025-02-15 11:51:14 +0000

    build image quietly

--- hndata-clj/load-data.sh
+++ hndata-clj/load-data.sh
@@ -7,14 +7,13 @@ set +a
 
 # Build Docker Image
 echo "Building Docker image..."
-docker build -t hn-trends-load .
+docker build -q -t hn-trends-load .
 
 # Check if Docker build was successful
 if [ $? -ne 0 ]; then
     echo "Docker build failed, exiting."
     exit 1
 fi
-echo "Docker image built successfully."
 
 # Construct the JDBC URL
 DATABASE_URL="jdbc:postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@db:5432/hndata"
